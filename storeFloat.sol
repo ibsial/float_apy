7d46a51c6e15bdac7c9568c4fc5f17de00b20dd3
ibsial <113923754+ibsial@users.noreply.github.com>
2022-12-22 16:17:03 +0300

    Update storeFloat.sol

--- storeFloat.sol
+++ storeFloat.sol
@@ -16,21 +16,21 @@ contract Float {
     bool initDone;
 
 
-    function addNumber(uint32 _slot, bytes32 _preparedNumber) public {
+    function addNumber(uint32 _slot, bytes32 _preparedNumber) internal {
         values[_slot] = _preparedNumber;
     }
 
-    function addNumbers(uint32[] memory _slots, bytes32[] memory _preparedNumbers) public {
+    function addNumbers(uint32[] calldata _slots, bytes32[] calldata _preparedNumbers) external {
         require(_slots.length == _preparedNumbers.length, "arrays length mismatch");
         for (uint i = 0; i < _slots.length; i++) {
             addNumber(_slots[i], _preparedNumbers[i]);
         }
     }
-    function addToken(address _token, PositionData memory _position) public {
+    function addToken(address _token, PositionData calldata _position) public {
             position[_token] = _position;
     }
 
-    function addTokens(address[] memory _token, PositionData[] memory _position) public {
+    function addTokens(address[] calldata _token, PositionData[] calldata _position) external {
         for (uint i = 0; i < _token.length; i++) {
             addToken(_token[i], _position[i]);
         }
@@ -38,7 +38,7 @@ contract Float {
 
 
     bytes32 public value;
-    function _getValue(uint128 _slot, uint _firstBytePos) public returns(bytes32) {
+    function _getValue(uint128 _slot, uint _firstBytePos) internal returns(bytes32) {
         // maybe input bit position to avoid multiplication to 8 ?
         value = (values[_slot] << (_firstBytePos * 8)) >> 240;
         return value;
@@ -48,7 +48,7 @@ contract Float {
     // consider mantissa taking first 2 bits
     uint public num;
     uint256 public result;
-    function _decodeValue(bytes32 _value) public returns (uint256) {
+    function _decodeValue(bytes32 _value) internal returns (uint256) {
         num = uint256(_value);
         uint256 mantissa = uint256(_value) >> 14;
         uint256 body = (uint256(_value) << 242) >> 242;
@@ -61,7 +61,7 @@ contract Float {
     // we want to mean a float number 101.01
     // these bits are converted to a hex number 
     // A775
-    function getValueForToken(address _token) public  returns (uint256) {
+    function getValueForToken(address _token) external returns (uint256) {
         PositionData memory pos = position[_token];
         value = _getValue(pos.slot, pos.fraction);
         return  _decodeValue(value);
